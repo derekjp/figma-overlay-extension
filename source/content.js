a147fb0cf4f1fbbd6ec45b8368643c368029a45c
derekjp <djp8543@gmail.com>
2023-05-07 13:15:50 -0400

    version 1

--- source/content.js
+++ source/content.js
@@ -3,14 +3,23 @@ import optionsStorage from './options-storage.js';
 console.log('💈 Content script loaded for', chrome.runtime.getManifest().name);
 async function init() {
 	const options = await optionsStorage.getAll();
-	const color = 'rgb(' + options.colorRed + ', ' + options.colorGreen + ',' + options.colorBlue + ')';
-	const text = options.text;
-	const notice = document.createElement('div');
-	notice.innerHTML = text;
-	document.body.prepend(notice);
-	notice.id = 'text-notice';
-	notice.style.border = '2px solid ' + color;
-	notice.style.color = color;
+	const embed = options.embed;
+	const opacity = options.opacity + '%';
+	const figmaFrame = document.createElement('div');
+	document.body.prepend(figmaFrame);
+	figmaFrame.id = 'iframe-figmaFrame';
+	figmaFrame.innerHTML = embed;
+	figmaFrame.style.position = 'fixed';
+	figmaFrame.style.top = '0';
+	figmaFrame.style.left = '0';
+	figmaFrame.style.bottom = '0';
+	figmaFrame.style.right = '0';
+	figmaFrame.style.width = '100%';
+	figmaFrame.style.height = '100%';
+	figmaFrame.style.opacity = opacity;
+	figmaFrame.style.zIndex = '9999999';
 }
 
 init();
+
+
